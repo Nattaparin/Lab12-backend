f9afe06920cf4015f8601d544d89708bf0316e34
Nattaparin <nattaparin455@gmail.com>
2022-10-17 03:24:54 +0700

    3

--- src/main/java/se331/rest/service/OrganizerServiceImpl.java
+++ src/main/java/se331/rest/service/OrganizerServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import se331.rest.dao.OrganizerDao;
+import se331.rest.entity.Event;
 import se331.rest.entity.Organizer;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -16,12 +18,25 @@ public class OrganizerServiceImpl implements OrganizerService{
     OrganizerDao organizerDao;
     @Override
     public List<Organizer> getAllOrganizer() {
-               return organizerDao.getOrganizer(Pageable.unpaged()).getContent();
-           }
+        return organizerDao.getOrganizer(Pageable.unpaged()).getContent();
+    }
 
-           @Override
+    @Override
     public Page<Organizer> getOrganizer(Integer page, Integer pageSize) {
-              return organizerDao.getOrganizer(PageRequest.of(page,pageSize));
-          }
+        return organizerDao.getOrganizer(PageRequest.of(page-1,pageSize));
+    }
+
+
+    @Override
+    @Transactional
+    public Organizer save(Organizer organizer) {
+
+        return organizerDao.save(organizer);
+    }
+
+    @Override
+    public Organizer getOrganizer(Long id) {
+        return organizerDao.getOrganizer(id);
+    }
 }
 
